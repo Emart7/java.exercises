d3ee32306926d85485eb5ee2123612c7f082e1e3
Emart7 <zaiyayin@hotmail.com>
2021-12-26 19:38:34 -0500

    Exercices

---
+++ src/Literales.java
@@ -0,0 +1,39 @@
+public class Literales {
+    public static void main(String[] args) {
+        // Declaracion de variables
+
+        int enteroDecimal = 100;
+        int enteroOctal = 0100;
+        int enteroHexadecimal = 0x100;
+
+        System.out.println("Valor entero decimal: " + enteroDecimal);
+        System.out.println("Valor entero octal: " + enteroOctal);
+        System.out.println("Valor entero hexadecimal: " + enteroHexadecimal);
+
+        long numeroLong = 100L;
+        System.out.println("Valor long: " + numeroLong);
+
+        double numeroDouble = 2.5;
+        System.out.println("Valor double: " + numeroDouble);
+
+        float numeroFLoat = 2.5f;
+        System.out.println("Valor float: " + numeroFLoat);
+
+        int numeroConGuiones = 25_258_236;
+        System.out.println("Valor con guiones: " + numeroConGuiones);
+
+        char miChar = 'b';
+        System.out.println("El caracter char es: " + miChar);
+
+        char miChar2 = '\u0062';
+        System.out.println("El caracter miChar2 es: " + miChar2);
+
+        char comillaSimple = '\'';
+
+        String stringLiteral = "Esto es un literal";
+
+        boolean booleanoVerdadero = true;
+
+        String stringNulo = null;
+    }
+}

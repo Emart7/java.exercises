ffefc538e441179eec64419ebc1954d4496a21aa
Emart7 <zaiyayin@hotmail.com>
2021-12-27 01:41:00 -0500

    Exercices

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,27 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        System.out.println("False & False es " + (false & false));
+        System.out.println("True & False es " + (true & false));
+        System.out.println("False & True es " + (false & true));
+        System.out.println("True & True es " + (true & true));
+        System.out.println("And");
+        System.out.println("False && False es " + (false && false));
+        System.out.println("True && False es " + (true && false));
+        System.out.println("False && True es " + (false && true));
+        System.out.println("True && True es " + (true && true));
+        System.out.println("Or");
+        System.out.println("False | False es " + (false | false));
+        System.out.println("True | False es " + (true | false));
+        System.out.println("False | True es " + (false | true));
+        System.out.println("True | True es " + (true | true));
+        System.out.println("x Or");
+        System.out.println("False ^ False es " + (false ^ false));
+        System.out.println("True ^ False es " + (true ^ false));
+        System.out.println("False ^ True es " + (false ^ true));
+        System.out.println("True ^ True es " + (true ^ true));
+
+        System.out.println("!False es: " + !false);
+        System.out.println("!True es: " + !true);
+
+    }
+}
